198307880662bf5551e2e5cd29bf318c72ca6503
Aleksahappy <aleksa.happy@gmail.com>
2021-05-31 16:11:43 +0400

    add edits

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -8,15 +8,24 @@ export default function ShopCard({item}) {
 
   const {url, MainImage, title, currency_code = '', price = '', quantity = 0} = item;
   const shortTitle = title?.length > 49 ? title.slice(0, 50) + '...' : title;
+
   let cost;
   if (['USD', 'EUR'].includes(currency_code)) {
     cost = currency_code === 'USD' ? `$${price}` : `€${price}`;
   } else {
     cost = `${price} ${currency_code}`;
   }
-  const quantityClass = quantity <= 10 ? 'level-low' :
-                        quantity <= 20 ? 'level-medium' :
-                        'level-high';
+
+  let quantityClass = '';
+  if (quantity) {
+    if (quantity <= 10) {
+      quantityClass = 'low';
+    } else if (quantity <= 20) {
+      quantityClass = 'medium';
+    } else {
+      quantityClass = 'high';
+    }
+  }
 
   return (
     <div className="item">
@@ -28,7 +37,7 @@ export default function ShopCard({item}) {
       <div className="item-details">
         <p className="item-title">{shortTitle}</p>
         <p className="item-price">{cost}</p>
-        <p className={`item-quantity ${quantityClass}`}>{quantity} left</p>
+        <p className={`item-quantity level-${quantityClass}`}>{quantity} left</p>
       </div>
     </div>
   )
